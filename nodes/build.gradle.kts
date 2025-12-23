cabeb0786e42731c3c651f4a6cd8df294fad410d
d_z_4 <lunap@tuta.io>
2025-12-23 15:29:00 +0000

    replace paper with spigot, and add minestom in build.gradle.kts

--- nodes/build.gradle.kts
+++ nodes/build.gradle.kts
@@ -4,34 +4,20 @@ val outputJarName = "nodes"
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "2.3.0-RC"
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
 }
 
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://repo.dmulloy2.net/nexus/repository/public/")
 }
 
 dependencies {
-    compileOnly("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
     compileOnly("com.google.code.gson:gson:2.13.2")
 
-    paperweight.paperDevBundle("1.21.10-R0.1-SNAPSHOT")
-}
+    implementation("org.spigotmc:spigot-api:1.21.11-R0.1-SNAPSHOT")
 
-tasks {
-    assemble {
-        dependsOn(reobfJar)
-    }
-
-    named("reobfJar") {
-        dependsOn(jar)
-    }
-
-    jar {
-        archiveBaseName.set(outputJarName)
-    }
+    implementation("net.minestom:minestom:2025.12.20c-1.21.11")
 }
