6c0e4c51c986604cd6e657fc9d2c6be4f3a0bd1a
d_z_4 <lunap@tuta.io>
2026-01-24 22:52:56 +0000

    test: add performance indicator bossbar in test

--- src/test/kotlin/luna/nodes/NodesTest.kt
+++ src/test/kotlin/luna/nodes/NodesTest.kt
@@ -1,17 +1,23 @@
 package luna.nodes
 
 import luna.nodes.objects.TerritoryId
+import net.kyori.adventure.bossbar.BossBar
+import net.kyori.adventure.text.Component
 import net.minestom.server.MinecraftServer
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.entity.GameMode
 import net.minestom.server.event.EventNode
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
 import net.minestom.server.event.player.PlayerBlockInteractEvent
+import net.minestom.server.event.player.PlayerSpawnEvent
+import net.minestom.server.event.server.ServerTickMonitorEvent
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import java.nio.file.Paths
+import kotlin.math.floor
+import kotlin.math.min
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
@@ -32,11 +38,17 @@ class NodesTest {
 
         MinecraftServer.getGlobalEventHandler().addChild(eventNode)
 
+        val bossBar = BossBar.bossBar(Component.empty(), 1f, BossBar.Color.GREEN, BossBar.Overlay.PROGRESS)
+
         eventNode.addListener(AsyncPlayerConfigurationEvent::class.java) { event ->
             val player = event.player
             event.spawningInstance = instance
             player.respawnPoint = Pos(27000.0, 60.0, 5700.0)
             player.gameMode = GameMode.CREATIVE
+        }
+
+        eventNode.addListener(PlayerSpawnEvent::class.java) { event ->
+            event.player.showBossBar(bossBar);
         }
 
         eventNode.addListener(PlayerBlockInteractEvent::class.java) { event ->
@@ -46,6 +58,26 @@ class NodesTest {
                 Message.error(event.player, "interact event cancelled")
             }
         }
+
+        eventNode.addListener(ServerTickMonitorEvent::class.java, { e ->
+            val tickTime = floor(e.getTickMonitor().getTickTime() * 100.0) / 100.0
+            val runtime = Runtime.getRuntime()
+            val usedMemory = (runtime.totalMemory() - runtime.freeMemory()) / 1024 / 1024
+            val maxMemory = runtime.maxMemory() / 1024 / 1024
+
+            bossBar.name(
+                Component.text()
+                    .append(Component.text("MSPT: $tickTime | Mem: ${usedMemory}MB/${maxMemory}MB"))
+            )
+            bossBar.progress(min(tickTime / MinecraftServer.TICK_MS, 1.0).toFloat())
+
+            if (tickTime > MinecraftServer.TICK_MS) {
+                bossBar.color(BossBar.Color.RED)
+            } else {
+                bossBar.color(BossBar.Color.GREEN)
+            }
+        })
+
 
         // create test config
         val config = NodesConfig(
